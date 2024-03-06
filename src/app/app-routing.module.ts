c1a21406370d702217811d7799687c13f2b7119e
fantad007 <dungndm232@gmail.com>
2024-03-06 22:45:56 +0700

    second commit
    Add categories component and add footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WalletsComponent } from './wallets/wallets.component';
+import { CategoriesComponent } from './categories/categories.component';
 
-const routes: Routes = [{ path: 'wallets', component: WalletsComponent }];
+const routes: Routes = [
+  { path: 'wallets', component: WalletsComponent },
+  { path: 'categories', component: CategoriesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/management/management.module.ts
+++ src/app/management/management.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WalletsComponent } from '../wallets/wallets.component';
+import { CategoriesComponent } from '../categories/categories.component';
 
 
 
 @NgModule({
   declarations: [
-    WalletsComponent
+    WalletsComponent,
+    CategoriesComponent
   ],
   imports: [
     CommonModule
